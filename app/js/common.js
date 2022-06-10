0b3e41fbe40edc2e6872ead4cf614ca181a420d6
Viktoria <zaharova181114@gmail.com>
2022-06-10 11:30:03 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -27,17 +27,17 @@ $(window).on('load resize', function() {
     if ($(window).width() < 992) {
         $(".go_to").on("click", function (event) {
             //отменяем стандартную обработку нажатия по ссылке
-            event.preventDefault();
-
-            //забираем идентификатор бока с атрибута href
-            var id = $(this).attr('href'),
-
-                //узнаем высоту от начала страницы до блока на который ссылается якорь
-                top = $(id).offset().top,
-                header = $('.header-top').height();
-
-            //анимируем переход на расстояние - top за 500 мс
-            $('body,html').animate({scrollTop: top - header}, 500);
+            // event.preventDefault();
+            //
+            // //забираем идентификатор бока с атрибута href
+            // var id = $(this).attr('href'),
+            //
+            //     //узнаем высоту от начала страницы до блока на который ссылается якорь
+            //     top = $(id).offset().top,
+            //     header = $('.header-top').height();
+            //
+            // //анимируем переход на расстояние - top за 500 мс
+            // $('body,html').animate({scrollTop: top - header}, 500);
 
             $('.btn-burger').removeClass('click');
             $('.menu').fadeOut();
